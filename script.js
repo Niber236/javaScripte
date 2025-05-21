61378b72d16bf0276b2b2f4e4c02462154b7cf8a
Niber236 <berniskouzou5@gmail.com>
2025-05-21 14:50:06 +0200

    jjk
    ij

---
+++ script.js
@@ -0,0 +1,38 @@
+function getData() {
+   fetch('data.json')
+     .then((response) => {
+       if (!response.ok) {
+         throw new Error('Network response was not ok');
+       }
+       return response.json();
+     })
+     .then((data) => {
+       // Traitez les données comme vous le souhaitez
+       console.log(data)
+       /// ON ECRIT LE CODE ICI ! 
+        let containerNav = document.getElementById('nav')
+         console.log(containerNav);
+
+         let containerHeader = document.getElementById('header')
+         console.log(containerHeader);
+
+         let containerSectionOne = document.getElementById('sectionOne')
+         console.log(containerSectionOne);
+
+         let containerSectionDeux =document.getElementById('sectionDeux')
+         console.log(containerSectionDeux);
+         
+         
+         
+      
+
+      
+
+       /// FIN DU CODE
+     })
+     .catch((error) => console.error('Erreur lors de la lecture des données :', error));
+ }
+ 
+ getData();
+
+ ///ON écrit les fonctions ici
